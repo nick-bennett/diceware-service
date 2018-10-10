685e93a0c83f45db83f76e34fb980949000030da
Nick Bennett <nick@nickbenn.com>
2018-10-10 09:18:49 -0600

    Simple service.

---
+++ src/main/java/edu/cnm/deepdive/dicewareservice/DicewareController.java
@@ -0,0 +1,31 @@
+package edu.cnm.deepdive.dicewareservice;
+
+import edu.cnm.deepdive.cryptography.ArtifactGenerator;
+import edu.cnm.deepdive.cryptography.PassphraseGenerator;
+import edu.cnm.deepdive.cryptography.WordSource;
+import java.util.Random;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DicewareController {
+
+  private ArtifactGenerator generator;
+
+  public DicewareController(WordSource source, Random rng) {
+    generator = new PassphraseGenerator(source, rng);
+  }
+
+  @GetMapping(path = "/diceware", produces = MediaType.TEXT_PLAIN_VALUE)
+  public String get(@RequestParam(name = "length", defaultValue = "6") int length) {
+    return generator.generate(length);
+  }
+
+  @GetMapping(path = "/diceware", produces = MediaType.APPLICATION_JSON_VALUE)
+  public String[] getJson(@RequestParam(name = "length", defaultValue = "6") int length) {
+    return get(length).split("\\s+");
+  }
+
+}
